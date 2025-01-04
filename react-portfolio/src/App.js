725ba137d0d859395771a070d204de39788a1c5e
Michael <sharobeem11@hotmail.com>
2025-01-04 00:30:13 -0500

    created Project section and added img for about section

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Avatar } from './components/Avatar/Avatar';
 import { About } from './components/About/About';
 import { Experience } from './components/Experience/Experience';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Avatar />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
